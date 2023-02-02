3d2bd44f8d6463026953a1950dc4dd935f383670
Allie Soldau <alliesoldau@gmail.com>
2023-02-02 05:35:01 -0500

    figured out how to import colors into my styled components

---
+++ client/src/styles/Colors.js
@@ -0,0 +1,7 @@
+export const COLORS = {
+    'orange': '#fcaa67',
+    'red': '#b0413e',
+    'cream': '#ffffc7',
+    'teal': '#548687',
+    'purple': '#473335'
+}
--- client/src/styles/NavBarStyles.js
+++ client/src/styles/NavBarStyles.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
+import { COLORS } from './Colors';
 
 const NavBarStyles = styled.div`
+    
     display: flex;
     width: 100vw;
 
-    background-color: blue;
+    background-color: ${COLORS.purple};
     img {
         height: 80px;
         margin-left: 10px;
