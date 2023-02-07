da8b7da7371cb964ca923cd0313afae43a566e09
Allie Soldau <alliesoldau@gmail.com>
2023-02-07 14:34:50 -0500

    moving date picker to resform. for now i will only filter by other attributes

--- client/src/components/CamperComponents/Search/MapFilters.js
+++ client/src/components/CamperComponents/Search/MapFilters.js
@@ -1,37 +1,11 @@
-import React, { useState } from 'react';
-// import Calendar from 'react-calendar'
-// import 'react-calendar/dist/Calendar.css';
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-
-// date picker source: https://www.npmjs.com/package/react-datepicker 
-// https://reactdatepicker.com/
+import React from 'react';
 
 function MapFilters() {
 
-    const [startDate, setStartDate] = useState(new Date())
-    const [endDate, setEndDate] = useState(null)
-    const onChange = (dates) => {
-        const [start, end] = dates
-        setStartDate(start)
-        setEndDate(end)
-    }
-    console.log(startDate)
-    console.log(endDate)
-
     return (
-        <DatePicker
-        selected={startDate}
-        onChange={onChange}
-        startDate={startDate}
-        endDate={endDate}
-        minDate={new Date()}
-
-        // excludeDates={[addDays(new Date(), 1), addDays(new Date(), 5)]}
-        selectsRange
-        selectsDisabledDaysInRange
-        inline
-      />
+        <>
+            <p>MapFilters</p>
+        </>
     )
 }
 
--- client/src/components/CamperComponents/Search/ResForm.js
+++ client/src/components/CamperComponents/Search/ResForm.js
@@ -1,10 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function ResForm() {
+
+    // date picker source: https://www.npmjs.com/package/react-datepicker 
+// https://reactdatepicker.com/
+
+    const [startDate, setStartDate] = useState(new Date())
+    const [endDate, setEndDate] = useState(null)
+    const [startDateFormatted, setStartDateFormatted] = useState(null)
+    const [endDateFormatted, setEndDateFormatted] = useState(null)
+
+    const onChange = (dates) => {
+        const [start, end] = dates
+        setStartDate(start)
+        setEndDate(end)
+    }
+
+    function handleClick() {
+        const sDate = `${startDate.getFullYear()}-${startDate.getMonth()}-${startDate.getDate()}`
+        setStartDateFormatted(sDate)
+        const eDate = `${endDate.getFullYear()}-${endDate.getMonth()}-${endDate.getDate()}`
+        setEndDateFormatted(eDate)
+        console.log(sDate)
+        console.log(eDate)
+    }
     return (
         <>
-            <p>Res Form</p>
-        </>
+        <button onClick={handleClick}>Format Dates</button>
+        <DatePicker
+            selected={startDate}
+            onChange={onChange}
+            startDate={startDate}
+            endDate={endDate}
+            minDate={new Date()}
+            selectsRange
+            selectsDisabledDaysInRange
+            inline
+
+        // filter daays --> use this to block out days that there is no availabililty? 
+        // () => {
+        //     const [startDate, setStartDate] = useState(null);
+        //     const isWeekday = (date) => {
+        //       const day = getDay(date);
+        //       return day !== 0 && day !== 6;
+        //     };
+        //     return (
+        //       <DatePicker
+        //         selected={startDate}
+        //         onChange={(date) => setStartDate(date)}
+        //         filterDate={isWeekday}
+        //         placeholderText="Select a weekday"
+        //       />
+        //     );
+        //   };
+      />
+      </>
     )
 }
 
